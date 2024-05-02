0a5f870cfb6a724166d29dea72ee2166c6fcd860
Fikiling <fsg.filipegoncalves@gmail.com>
2024-05-02 18:19:08 +0100

    pequenas coisas

--- Projeto/analex_forth.py
+++ Projeto/analex_forth.py
@@ -1,69 +1,209 @@
 import ply.lex as lex
 
+#NOTA: Formula para forçar TXT a estar separado de outros tokens: (?<!\S)TXT(?!\S)
+
+# Palavras reservadas Forth
+reserved = {
+    'dup': 'DUP',
+    'drop': 'DROP',
+    'swap': 'SWAP',
+    'rot': 'ROT',
+    'over': 'OVER',
+    'tuck': 'TUCK',
+    'nip': 'NIP',
+    'neg': 'NEG',
+    'abs': 'ABS',
+    'min': 'MIN',
+    'max': 'MAX',
+    'and': 'AND',
+    'or': 'OR',
+    'xor': 'XOR',
+    'not': 'NOT',
+    'lshift': 'LSHIFT',
+    'rshift': 'RSHIFT',
+    'begin': 'BEGIN',
+    'until': 'UNTIL',
+    'emit': 'EMIT',
+    'cr': 'CR',
+    'spaces': 'SPACES',
+    'space': 'SPACE',
+    'char': 'CHAR',
+    'key': 'KEY',
+    'if': 'IF',
+    'else': 'ELSE',
+    'then': 'THEN',
+    'while': 'WHILE',
+    'repeat': 'REPEAT',
+    'do': 'DO',
+    'loop': 'LOOP',
+    'variable': 'VARIABLE',
+
+}
+
 # Lista de tokens soma, adição, subtração, divisão, resto da divisão inteira, potência
-tokens = [
+tokens = (
     'NUM',
     'SOMA',
     'SUBTRACAO',
     'DIVISAO',
     'MULTIPLICACAO',
     'RESTO',
-    'POTENCIA'
-]
+    'POTENCIA',
+    '2PONTOS',
+    'FUNCAO',
+    'PONTOVIRGULA',
+    'ID',
+    'ZEROEQUAL',
+    'ZEROLESS',
+    'ZEROGREAT',
+    'EQUAL',
+    'LESS',
+    'GREAT',
+    'POINT',
+    'POINTSTRING',
+    'DIVIDE_BY_2',
+) + tuple(reserved.values())
+
+states = (
+    ('beginF', 'exclusive'), 
+)
+
+def t_commentary(t):
+    r'\( .* \)'
+    pass
 
 def t_NUM(t):
-    r'\d+'
-    lex.num += 1
+    r'(?<!\S)\d+(?!\S)'
     t.value = int(t.value)
     return t
 
 def t_SOMA(t):
-    r'\+'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\+(?!\S)'
+    t.value = 'SOMA'
     return t
 
 def t_SUBTRACAO(t):
-    r'\-'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\-(?!\S)'
+    t.value = 'SUBTRACAO'
     return t
 
 def t_DIVISAO(t):
-    r'\/'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\/(?!\S)'
+    t.value = 'DIVISAO'
     return t
 
 def t_MULTIPLICACAO(t):
-    r'\*'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\*(?!\S)'
+    t.value = 'MULTIPLICACAO'
     return t
 
 def t_RESTO(t):
-    r'\%'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\%(?!\S)'
+    t.value = 'RESTO'
     return t
 
 def t_POTENCIA(t):
-    r'\^'
-    lex.num -= 1
-    if lex.num < 1: 
-        raise SyntaxError("Número de operadores maior ou igual que o número de operandos")
+    r'(?<!\S)\^(?!\S)'
+    t.value = 'POTENCIA'
+    return t
+
+def t_2PONTOS(t):
+    r'(?<!\S)\:(?!\S)'
+    t.value = '2PONTOS'
+    t.lexer.begin('beginF')
+    return t
+
+def t_beginF_FUNCAO(t):
+    r'(?<!\S)[a-zA-Z_][a-zA-Z_123456789]*(?!\S)'
+
+    if lex.flagFunction == 1: 
+        raise SyntaxError("Não é possível definir uma função dentro de outra função")
+
+    lex.flagFunction = 1
+    t.lexer.functions.append(t.value)
+    t.lexer.begin('INITIAL')
+    return t
+
+def t_PONTOVIRGULA(t):
+    r'(?<!\S)\;(?!\S)'
+    lex.flagFunction = 0
+    t.value = 'PONTOVIRGULA'
+    return t
+
+def t_ID(t):
+    r'(?<!\S)[a-zA-Z_][a-zA-Z_0-9]*(?!\S)'
+    if t.type == 'ID':
+        if (t.value in t.lexer.functions):
+            return t
+        elif (t.value not in reserved):
+            raise Exception(f'Function {t.value} is not defined.')
+        
+    return t
+
+def t_ZEROEQUAL(t):
+    r'(?<!\S)\=0(?!\S)'
+    return t
+
+def t_ZEROLESS(t):
+    r'(?<!\S)\<0(?!\S)'
+    return t
+
+def t_ZEROGREAT(t):
+    r'(?<!\S)\>0(?!\S)'
+    return t
+
+def t_EQUAL(t):
+    r'(?<!\S)\=(?!\S)'
+    return t
+
+def t_LESS(t):
+    r'(?<!\S)\<(?!\S)'
+    return t
+
+def t_GREAT(t):
+    r'(?<!\S)\>(?!\S)'
+    return t
+
+def t_POINT(t):
+    r'(?<!\S)\.(?!\S)'
+    return t
+
+def t_POINTSTRING(t):
+    r'(?<!\S)\s\.\"(.*)+\"(?!\S)'
+    return t
+
+def t_DIVIDE_BY_2(t):
+    r'(?<!\S)2\/(?!\S)'
     return t
 
 t_ignore = ' \n\t'
+t_beginF_ignore = ' \n\t'
 
 def t_error(t):
     print(f"Carácter ilegal {t.value[0]}")
     t.lexer.skip(1)
 
+def t_beginF_error(t):
+    print(f"Carácter ilegal {t.value[0]}")
+    t.lexer.skip(1)
+
 lex = lex.lex()
-lex.num = 0
+lex.begin('INITIAL')
+
+lex.functions = []
+lex.flagFunction = 0
+
+'''
+
+file = """
+: AVERAGE  + 2/ ;
+10 20 AVERAGE .
+do
+"""
+
+lex.input(file)
+
+while tok := lex.token():
+    print(tok)
+
+'''
--- Projeto/analsintax_forth.py
+++ Projeto/analsintax_forth.py
@@ -2,18 +2,59 @@ import ply.yacc as yacc
 from analex_forth import tokens
 import analex_forth as analex 
 
+functions = {}
+codigo_forth = []
+indice = -1
+
+# s : input FIM
 # input : input linha
 #       | empty
-# linha : expNum
-#       | função
-#       | print
+# linha : elem                                   
+#       | 2PONTOS funcao input PONTOVIRGULA
 #       | condicional
 #       | ciclo
 #       | variaveis
-# expNum : expNum fator
-#        | fator
-# fator : NUM
-#       | operador 
+# funcao : FUNCAO
+# elem : NUM                            DONE
+#      | operador                       DONE
+#      | ID                             DONE
+#      | POINT                          DONE
+#      | POINTSTRING    
+#      | CR
+#      | EMIT
+#      | CHAR
+#      | DROP
+#      | SWAP
+#      | ROT
+#      | OVER
+#      | TUCK
+#      | NIP
+#      | NEG
+#      | ABS
+#      | MIN
+#      | MAX
+#      | AND
+#      | OR
+#      | XOR
+#      | NOT
+#      | LSHIFT
+#      | RSHIFT
+#      | BEGIN
+#      | UNTIL
+#      | SPACES
+#      | SPACE
+#      | DUP
+#      | KEY
+# operador : SOMA               
+#          | SUBTRACAO
+#          | DIVISAO
+#          | MULTIPLICACAO
+#          | RESTO
+#          | POTENCIA
+#          | DIVIDE_BY_2
+#
+# empty :
+
 
 
 
@@ -23,28 +64,40 @@ def p_input(p):
 
 def p_input_empty(p):
     'input : empty'
+    p[0] = ""
+
+def p_linha_statement(p):
+    'linha : elem'
+    p[0] = p[1]
+
+def p_linha_funcao(p):
+    'linha : 2PONTOS funcao input PONTOVIRGULA'
+    global functions
+    functions[p[2].strip()] = p[3]
     p[0] = ''
 
-def p_linha_expNum(p):
-    'linha : expNum'
-    p[0] = p[1]
+def p_funcao(p):
+    'funcao : FUNCAO'
+    p[0] = p[1] + '\n'
     
-def p_expNum_composta(p):
-    'expNum : expNum fator'
-    p[0] = p[1] + p[2] 
-
-def p_expNum_simples(p):
-    'expNum : fator'
-    p[0] = p[1] 
-
-def p_fator_NUM(p):
-    'fator : NUM'
+def p_elem_NUM(p):
+    'elem : NUM'
     p[0] = 'pushi ' + str(p[1]) + '\n'
 
-def p_fator_operador(p):
-    'fator : operador'
+def p_elem_operador(p):
+    'elem : operador'
     p[0] = p[1]
 
+def p_elem_ID(p):
+    'elem : ID'
+    global functions
+    if p[1] not in functions:
+        raise Exception(f'Function |{p[1]}| is not defined.')
+    p[0] = 'pusha ' + p[1] + '\n' + 'call\n'
+
+def p_elem_POINT(p):
+    'elem : POINT'
+    p[0] = 'writei \n'
 
 def p_operador_SOMA(p):
     'operador : SOMA'
@@ -70,6 +123,10 @@ def p_operador_POTENCIA(p):
     'operador : POTENCIA'
     p[0] = 'pow\n'
 
+def p_operador_DIVIDE_BY_2(p):
+    'operador : DIVIDE_BY_2'
+    p[0] = 'pushi 2\n'+ 'div\n'
+
 def p_empty(p):
     'empty :'
     pass
@@ -80,11 +137,58 @@ def p_error(p):
     else:
         print("Erro de sintaxe no final do input")
 
+
+# -----------------------Minhas Funções----------------------------
+
+def input_multiline():
+    global codigo_forth
+    global indice
+    linha = ""
+    
+    while linha != "FIM":
+        linha = input()
+        if linha != "FIM":
+            codigo_forth.append(linha)
+            indice += 1
+    print("------------------FIM DE BLOCO-----------------")
+
+# -----------------------Rodar o Programa----------------------------
+
 parser = yacc.yacc()
 
-while s := input('Digite código FORTH > '):
-    print("\nstart\n")
-    analex.lex.num = 0
-    result = parser.parse(s)
-    print(result)
-    print("stop\n")
+'''
+print("Regras de escrita:")
+print("     FIM -> fim de bloco de código")
+print("     CTRL+D > termina o programa.")
+print("Digite seu código FORTH:")
+
+try:
+        while True:
+            # Receber input
+            input_multiline()
+            codigo_forth_completo = '\n'.join(codigo_forth)
+            
+            # Processar input
+            analex.lex.flagFunction = 0
+            result = parser.parse(codigo_forth_completo)
+            print(result)
+
+except EOFError:  # Captura Ctrl+D 
+        print("Ctrl+D recebido ---> Programa a encerrar.")
+'''
+
+analex.lex.flagFunction = 0
+with open("data.txt", "r") as file:
+    codigo = file.read()
+
+
+result = parser.parse(codigo)
+
+with open("output.txt", "w") as file:
+    file.write(result)
+    
+    file.write("\n")
+    for elem in functions:
+        file.write(elem + ":\n")
+        formatted_string = "\t" + functions[elem].replace("\n", "\n\t") + "return"
+        file.write(formatted_string)
